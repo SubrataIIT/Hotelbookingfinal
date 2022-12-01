64183636c1ce9c9b768c9cde5c9ee2864ad6035c
SubrataIIT <111726085+SubrataIIT@users.noreply.github.com>
2022-12-01 19:18:38 +0530

    Add files via upload

---
+++ project/components/footer.php
@@ -0,0 +1,39 @@
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +123-456-7890</a>
+         <a href="tel:8250040288"><i class="fas fa-phone"></i> +8250040288</a>
+         <a href="mailto:subratadas@iitg.ac.in"><i class="fas fa-envelope"></i> subratadas@iitg.ac.in</a>
+         <a href="mailto:anasbhai@gmail.com"><i class="fas fa-envelope"></i> paradise@iitg.ac.in</a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i> Guwahati, india - 781039</a>
+      </div>
+
+      <div class="box">
+         <a href="#home">home</a>
+         <a href="#about">about</a>
+         <a href="bookings.php">my bookings</a>
+         <a href="#reservation">reservation</a>
+         <a href="#gallery">gallery</a>
+         <a href="#contact">contact</a>
+         <a href="#reviews">reviews</a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/subrata.dasbabu.75">facebook <i class="fab fa-facebook-f"></i></a>
+         <a href="">twitter <i class="fab fa-twitter"></i></a>
+         <a href="">instagram <i class="fab fa-instagram"></i></a>
+         <a href="https://www.linkedin.com/in/subrata-das-737203235/">linkedin <i class="fab fa-linkedin"></i></a>
+         <a href="https://www.youtube.com/results?search_query=hotel+booking">youtube <i class="fab fa-youtube"></i></a>
+      </div>
+
+   </div>
+
+   <div class="credit">&copy; copyright @ 2022 by mr. subrata | all rights reseved!</div>
+
+</section>
+
+<!-- footer section ends -->
